20cc4e691092faada6dd3d77cbe369d494e124aa
Gooch, Justin <JUSTIN_GOOCH@homedepot.com>
2019-10-19 14:40:12 -0400

    added initial methods for genserver to run every 12 hours and on initiation of application

--- lib/where_is/users_genserver.ex
+++ lib/where_is/users_genserver.ex
@@ -1,41 +1,41 @@
-defmodule Users do
+defmodule WhereIs.Users do
   use GenServer
+
+ def start_link(_) do
+    GenServer.start_link(__MODULE__, %{})
+  end
 
 
   # Callbacks
 
-#   @impl true
-#   def init(stack) do
-#     {:ok, stack}
-#   end
+   @impl true
+   def init(state) do
+    schedule_matter_most_user_call()
+     {:ok, state}
+  end
 
-#   @impl true
-#   def handle_call(:pop, _from, [head | tail]) do
-#     {:reply, head, tail}
-#   end
 
-#   @impl true
-#   def handle_cast({:push, element}, state) do
-#     {:noreply, [element | state]}
-#   end
-# end
+  def handleInfo(:work, state) do 
 
-# # Start the server
-# {:ok, pid} = GenServer.start_link(Stack, [:hello])
-
-# # This is the client
-# GenServer.call(pid, :pop)
-# #=> :hello
-
-# GenServer.cast(pid, {:push, :world})
-# #=> :ok
-
-# GenServer.call(pid, :pop)
-# #=> :world
+    #call method to get userInfo 
+    
+    #upload userInfo to database. 
 
 
 
-#call mattermost users every 12 hours
+    #after doing the work; schedule more work 
+    #kind of like sisyphus if you think about it; 
+    #you push the proverbial boulder up a hill, only for it to fall down again; and in 12 hours; 
+    #you push the boulder up again
+    schedule_matter_most_user_call()
+
+    {:noreply, state} 
+  end
+
+  #call mattermost users every 12 hours
+  defp schedule_matter_most_user_call() do
+    Process.send_after(self(), :work, 12 * 60 * 60 * 1000)
+  end
 
 #
 end
