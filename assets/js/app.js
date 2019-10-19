c638a324fceb2931186f3d80e3a2f12364ed8cbd
Roland <rolandcanuto@outlook.com>
2019-10-18 22:47:02 -0400

    merged vincents changes

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,21 +1,5 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
 // We need to import the CSS so that webpack will load it.
 // The MiniCssExtractPlugin is used to separate it out into
 // its own CSS file.
 import resetCss from '../css/reset.css'
 import css from "../css/app.css"
-
-import Header from './components/Header/Header';
-import Main from './components/Main/Main';
-
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Main />
-    </div>
-  );
-}
-
-ReactDOM.render(<App />, document.getElementById('mountPoint'))
