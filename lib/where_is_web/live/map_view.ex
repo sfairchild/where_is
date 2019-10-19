a961b22c5daa86222ea7ac81982de6fc255d22e8
Roland <rolandcanuto@outlook.com>
2019-10-19 16:31:58 -0400

    commiting changes

--- lib/where_is_web/live/map_view.ex
+++ lib/where_is_web/live/map_view.ex
@@ -5,13 +5,31 @@ defmodule WhereIsWeb.MapLive do
     WhereIsWeb.PageView.render("map.html", assigns)
   end
 
+  defmodule User do
+    defstruct name: "Roland Canuto", email: "rolandcanuto@outlook.com", username: "rolandc5"
+  end
+
   def mount(_session, socket) do
     socket = socket
-             |> assign(:svg, WhereIs.Svg.generate_svg)
+            |> assign(:name, "")
+            |> assign(:email, "")
+            |> assign(:username, "")
+            |> assign(:svg, WhereIs.Svg.generate_svg)
     {:ok, socket}
   end
 
   def handle_event("search", %{"value" => value}, socket) do
     {:noreply, assign(socket, value: value)}
   end
+
+  def handle_event("mockData", _, socket) do
+    user = %User{}
+    socket = socket
+      |> assign(:name, user.name)
+      |> assign(:email, user.email)
+      |> assign(:username, user.username)
+    {:noreply, socket}
+  end
+
+
 end
