acebe252d06f64de12d59db1192144826f56df83
1VinceP <vpalmergraphics@gmail.com>
2019-10-18 21:26:20 -0400

    home page base layout

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+// We need to import the CSS so that webpack will load it.
+// The MiniCssExtractPlugin is used to separate it out into
+// its own CSS file.
+import resetCss from '../css/reset.css'
+import css from "../css/app.css"
 
 const ReactOnPhoenix = () => <div>This is a React Component!!!</div>
 
