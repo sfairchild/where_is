dfd74d07d2145812f61955dc7ce37ed26071d1f8
1VinceP <vpalmergraphics@gmail.com>
2019-10-20 15:57:14 -0400

    add semicolons to app.js because we aren't peasants

--- assets/js/app.js
+++ assets/js/app.js
@@ -21,15 +21,15 @@ import "phoenix_html"
 import svgPanZoom from "svg-pan-zoom"
 
 let Hooks = {};
-let pan
+let pan;
 
 Hooks.PanZoom = {
   mounted() {
     pan = svgPanZoom('#mainSvg');
   },
   updated() {
-    pan.panBy({x: 0.01, y: 0.01})
-  }
+    pan.panBy({x: 0.01, y: 0.01});
+  },
 };
 
 Hooks.TitleName = {
