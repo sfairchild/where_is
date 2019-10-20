ef26e8884452617f3d3a0a8621b40c3f0f5fbe09
John Clough <jclough@LT-C02YC1W9JG5M.local>
2019-10-20 08:31:01 -0400

    updating assign variable

--- lib/where_is/mattermost_user.ex
+++ lib/where_is/mattermost_user.ex
@@ -1,7 +1,7 @@
 defmodule WhereIs.MattermostUser do
  @enforce_keys [:id, :username, :email]
- defstruct id: "", username: "", first_name: "", last_name: "", email: "", location_id: ""
-  # defstruct id: "", username: "", first_name: "", last_name: "", email: "", nickname: "", position: "", auth_data: "", auth_service: "", roles: [], timezone: %{automatic_timezone: "", manual_timezone: "", use_automaticTimezone: ""}, create_at: "", update_at: "", delete_at: ""
+ # defstruct id: "", username: "", first_name: "", last_name: "", email: "", location_id: ""
+ defstruct id: "", username: "", first_name: "", last_name: "", email: "", nickname: "", position: "", auth_data: "", auth_service: "", roles: [], timezone: %{automatic_timezone: "", manual_timezone: "", use_automaticTimezone: ""}, create_at: nil, update_at: "", delete_at: "", location_id: ""
 	alias WhereIs.MattermostUser, as: MattermostUser 
 
 
@@ -10,9 +10,10 @@ defmodule WhereIs.MattermostUser do
   	user = createUser(userMap, keys)
   	# IO.inspect(user)
 
-  	#fTODO: fix when we have more time
-  	# buildUserFromMap(keys, userMap, user)
-  	# IO.inspect(user)
+  	# fTODO: fix when we have more time
+  	IO.inspect(user)
+  	buildUserFromMap(keys, userMap, user)
+  	
   end
 
   def makeUsers(users) do
@@ -50,9 +51,8 @@ defmodule WhereIs.MattermostUser do
 
   defp buildUserFromMap([head | tail], userMap, user) do
   	IO.inspect(head)
+
   	{value, userMap} = Map.pop(userMap, head)
-  	{value, userMap} = Map.pop(userMap, head)
-  	IO.inspect(userMap)
   	IO.inspect(value)
   	user = assignVariable(head, value, user)
 
@@ -64,8 +64,19 @@ defmodule WhereIs.MattermostUser do
   end
 
   defp assignVariable(key, value, user) do
-  		Map.put(user, key, value)
-  end
+  	#This doesn't actually work but does not break it.
+  		cond do
+			Map.has_key?(user, key) ->
+			IO.inspect(key, label: "assignVariable, key, true")
+
+  			user = %{user | key => value}
+
+  			1 = 1 ->
+  			 IO.inspect(key, label: "assignVariable, key, false")
+
+  				user
+  		end
+  	end
 
   def fetchCurrentMattermostUsersList do
     {:ok, users} = fetchUsersFromMattermost()
@@ -82,10 +93,7 @@ defmodule WhereIs.MattermostUser do
 	{:ok, response} = HTTPoison.get(url, headers)
 	Jason.decode(response.body())
   end
-
-
-
-
-
+#TODO: add locationID to users
+#TODO: Get SVG displaying to Mattermost
 end
 
